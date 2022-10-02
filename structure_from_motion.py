b3d971813c88cac49556a94705dd275b6933408c
Chethan Chinder C <chinder99@gmail.com>
2022-10-02 00:09:59 -0700

    changes in structure frm motion file

--- structure_from_motion.py
+++ structure_from_motion.py
@@ -1,9 +1,6 @@
-import numpy as np
-import cv2
-import os
 from baseline import Baseline
-import open3d
 from motion_utils import *
+from structureFromMotion_python.bundle_adjust import BundleAdjustment
 
 class Structure:
 
@@ -18,6 +15,11 @@ class Structure:
 		self.reconstructed_count = 0
 		self.points_3d = np.zeros((0,3))
 		self.colors = np.zeros((0,3))
+		self.point3d_indices = []
+		self.camera_indices = []
+		self.camera_matrices  = []
+		self.completed_pixel_points =[]
+		self.BA_rate = 3
 		for view in views:
 			self.names.append(view.image_name)
 		self.results_path = os.path.join(self.views[0].root_dir, 'reconstructed')
@@ -29,6 +31,8 @@ class Structure:
 			match = self.matches[(view1.image_name, view2.image_name)]
 			baseline = Baseline(view1, view2, match, self.K)
 			view2.rotation , view2.translation =  baseline.getPose()
+			self.camera_matrices.append(np.hstack((view1.rotation, view1.translation)))
+			self.camera_matrices.append(np.hstack((view2.rotation, view2.translation)))
 			rpe1, rpe2= self.triangulate(view1, view2)
 			self.errors.append(np.mean(rpe1))
 			self.errors.append(np.mean(rpe2))
@@ -37,6 +41,7 @@ class Structure:
 
 		else:
 			view1.rotation, view1.translation = self.pnp_projection(view1)
+			self.camera_matrices.append(np.hstack((view1.rotation, view1.translation)))
 			errors = []
 			for id, old_view in enumerate(self.completed):
 				match = self.matches[(old_view.image_name,view1.image_name)]
@@ -55,7 +60,8 @@ class Structure:
 
 
 	def remove_mapped_points(self, match, img_idx):
-		## get inliers not included in point map
+		## Removes inliers which are already constructed in the completed views
+
 		inliers1 = []
 		inliers2 = []
 		for i, (inlier1, inlier2) in enumerate(zip(match.inliers1,match.inliers2)):
@@ -74,6 +80,18 @@ class Structure:
 		for i, view in enumerate(self.views[2:]):
 			print(f"[structure from motion] reconstructing from view {i + 2} usng pnp")
 			self.compute_pose(view)
+			## bundle adjust every {BA_rate} frames
+			if(i%self.BA_rate==0):
+				print(f" Bundle Adjusting ...")
+				ba = BundleAdjustment(self.points_3d, np.array(self.completed_pixel_points), self.camera_matrices, self.K, self.names, self.camera_indices, self.point3d_indices)
+				camera_matrices, self.points_3d =ba.bundle_adjust()
+				self.camera_matrices = []
+				for j,camera_matrice in enumerate(camera_matrices):
+					R=camera_matrice[:,:3].reshape(3,3)
+					T = camera_matrice[:,3].reshape(3,1)
+					self.completed[j].rotation = R
+					self.completed[j].translation = T
+					self.camera_matrices.append(camera_matrice)
 			self.plot_points()
 
 	def plot_points(self):
@@ -121,6 +139,7 @@ class Structure:
 		reprojection_error1 = []
 		reprojection_error2 = []
 
+
 		for id, pt1 in enumerate(pts1_homg):
 			u1_norm = np.matmul( K_inv ,pt1)
 			u2_norm = np.matmul(K_inv ,pts2_homg[id])
@@ -129,11 +148,18 @@ class Structure:
 
 			self.colors = np.concatenate((self.colors, color),axis =0 )
 			self.points_3d = np.concatenate((self.points_3d, point_3d.T), axis=0)
+
 			reprojection_error1.append(calculateReprojectionError(view1.rotation, view1.translation, self.K, pt1[0:2], point_3d))
 			reprojection_error2.append(calculateReprojectionError(view2.rotation, view2.translation, self.K, pts2_homg[id][0:2], point_3d))
 			self.point_map[(self.names.index(view1.image_name), match.inliers1[id])] = self.reconstructed_count
 			self.point_map[(self.names.index(view2.image_name), match.inliers2[id])] = self.reconstructed_count
-			self.reconstructed_count+=1
+			self.completed_pixel_points.append(pt1[0:2])
+			self.completed_pixel_points.append(pts2_homg[id][0:2])
+			self.camera_indices.append(self.names.index(view1.image_name))
+			self.camera_indices.append(self.names.index(view2.image_name))
+			self.point3d_indices.append(self.reconstructed_count)
+			self.point3d_indices.append(self.reconstructed_count)
+			self.reconstructed_count += 1
 		return reprojection_error1, reprojection_error2
 
 	def pnp_projection(self, new_view):
